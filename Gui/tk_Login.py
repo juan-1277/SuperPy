2ad2eaca17285f0ffb3597091dcf47930a216d62
MaxiC0303 <maximiliano.ecamano@gmail.com>
2022-12-04 18:05:45 -0300

    Update tk_Login.py

--- Gui/tk_Login.py
+++ Gui/tk_Login.py
@@ -1,1 +1,18 @@
 import tkinter
+ventana = tkinter.Tk()
+ventana.geometry("400x300")
+
+boton1 = tkinter.Button(ventana, text = "Productos", bg = "orange")
+boton1.pack()
+
+boton2 = tkinter.Button(ventana, text = "Usuarios", bg = "orange")
+boton2.pack()
+
+boton3 = tkinter.Button(ventana, text = "Ventas", bg = "orange")
+boton3.pack()
+
+boton4 = tkinter.Button(ventana, text = "Salir", bg = "orange")
+boton4.pack()
+
+
+ventana.mainloop()
