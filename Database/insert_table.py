b17cd21aa056bf4d07c10e927373e6819f0b4800
MaxiC0303 <maximiliano.ecamano@gmail.com>
2022-12-10 13:12:42 -0300

    test

--- Database/insert_table.py
+++ Database/insert_table.py
@@ -6,5 +6,7 @@ db.insert("rol","nombre,descripcion","'Administrador','Administrador del sistema
 
 db.insert("usuario","nombre,apellido,dni,email,password,idrol","'Llatser','Juan','12345678','juanllatser@gmail.com','1277',1")
 
+db.insert("usuario","nombre,apellido,dni,email,password,idrol","'Camaño','Maxi','99999','maxicamano@gmail.com','3333',1")
+#para agregar un usuario nuevo simplemente agrego un db.insert("nombre de tabla", "las columnas de las tablas a insertar", datos de cada columna)
 
 db.close()
--- Gui/tk_Carrito.py
+++ Gui/tk_Carrito.py
@@ -11,34 +11,47 @@ producto = StringVar()
 #Definicion de funciones
 def agregar_productos():
    lista_productos.insert(END, producto.get())
-lista_productos = list()
+#lista_productos = list()
 
-def eliminar_productos(posicion):
-    lista_productos.delete(posicion)
+def agregar_cantidad():
+    lista_cantidades.insert(END, cantidad_entry.get())
+
+def eliminar_productos(item):
+    lista_productos.delete(item)
 
 #Creaciòn de Widgets
 titulo_label = tkinter.Label(ventana_carrito, text = "Carro de compras")
-total_label = tkinter.Label(ventana_carrito, text = "TOTAL A PAGAR $")
-lista_label = tkinter.Label(ventana_carrito, text = "LISTA DE PRODUCTOS")
-agregar_boton = tkinter.Button(ventana_carrito, text = "AGREGAR", command = agregar_productos)
+total_label = tkinter.Label(ventana_carrito, text = "Total a pagar $")
+lista_label = tkinter.Label(ventana_carrito, text = "Lista de productos")
+cantidades_label = tkinter.Label(ventana_carrito, text = "Cantidades")
+agregar_producto_boton = tkinter.Button(ventana_carrito, text = "AGREGAR", command = agregar_productos)
+agregar_cantidad_boton = tkinter.Button(ventana_carrito, text = "AGREGAR Q", command = agregar_cantidad)
 buscar_boton = tkinter.Button(ventana_carrito, text = "BUSCAR")
-buscar_label = tkinter.Label(ventana_carrito , text = "Ingresar un producto")
+ingresar_label = tkinter.Label(ventana_carrito , text = "Ingresar un producto")
+cantidad_label = tkinter.Label(ventana_carrito , text = "Ingresar cantidad")
+cantidad_entry = tkinter.Spinbox(ventana_carrito)
 txt_producto = tkinter.Entry(ventana_carrito, textvariable = producto)
 lista_productos = tkinter.Listbox(ventana_carrito)
+lista_cantidades = tkinter.Listbox(ventana_carrito)
 eliminar_label = tkinter.Label(ventana_carrito, text = "Pos. a Eliminar")
-eliminar_entry = tkinter.Entry(ventana_carrito)
-eliminar_boton = tkinter.Button(ventana_carrito, text = "ELIMINAR", command = eliminar_productos(eliminar_entry +1))
+eliminar_entry = tkinter.Spinbox(ventana_carrito)
+eliminar_boton = tkinter.Button(ventana_carrito, text = "ELIMINAR", command = eliminar_productos)
 
 
 #Colocaciòn de Widgets en pantalla
 titulo_label.grid(row = 0, column = 3)
-total_label.grid(row = 1, column = 10)
+total_label.grid(row = 1, column = 6)
 lista_label.grid(row = 2, column = 1)
-buscar_label.grid(row = 4, column = 3)
-txt_producto.grid(row = 4, column = 8)
-agregar_boton.grid(row = 4, column = 12)
-buscar_boton.grid(row = 4, column = 10)
+cantidades_label.grid(row = 2, column = 2)
+agregar_producto_boton.grid(row = 4, column = 7)
+agregar_cantidad_boton.grid(row = 5, column = 6)
+buscar_boton.grid(row = 4, column = 6)
+ingresar_label.grid(row = 4, column = 3)
+cantidad_label.grid(row = 5, column = 3)
+cantidad_entry.grid(row = 5, column = 5)
+txt_producto.grid(row = 4, column = 5)
 lista_productos.grid(row = 5, column = 1)
+lista_cantidades.grid(row = 5, column = 2)
 eliminar_label.grid(row = 7, column = 1)
 eliminar_entry.grid(row = 7, column = 2)
 eliminar_boton.grid(row = 7, column = 3)
