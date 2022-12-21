dbdb2b24d75e0057d1e76f9fe22ab23c5af20b82
MaxiC0303 <maximiliano.ecamano@gmail.com>
2022-12-20 21:38:15 -0300

    extras

--- Gui/tk_Carrito.py
+++ Gui/tk_Carrito.py
@@ -9,7 +9,7 @@ from Gui.tk_Productos import catalogo
 
 def carrito():    
     ventana_carrito = tkinter.Tk()
-    ventana_carrito.geometry("800x800")
+    ventana_carrito.geometry("15000x8000")
     ventana_carrito.title("Carro de compras")
     ventana_carrito.configure(bg = "blue4")
     #resultado = 0 
@@ -80,10 +80,15 @@ def carrito():
     titulo_label = tkinter.Label(ventana_carrito, text = "Carro de compras")
     total_label = tkinter.Label(ventana_carrito, text = "Total a pagar $")
     total_monto_label = tkinter.Entry(ventana_carrito)
+
     lista_label = tkinter.Label(ventana_carrito, text = "Lista de productos")
     cantidades_label = tkinter.Label(ventana_carrito, text = "Cantidades")
+    lista_parciales = tkinter.Label(ventana_carrito, text = "Monto Parcial")
+
     #precio_venta_label = tkinter.Label(ventana_carrito, text = "Precio")
     agregar_producto_boton = tkinter.Button(ventana_carrito, text = "AGREGAR", command = lambda: agregar_productosycantidades(txt_producto.get()), bg = "SkyBlue1")
+    
+    
     #agregar_cantidad_boton = tkinter.Button(ventana_carrito, text = "AGREGAR Q", command = agregar_cantidad)
     ingresar_label = tkinter.Label(ventana_carrito , text = "Ingresar un producto")
     cantidad_label = tkinter.Label(ventana_carrito , text = "Ingresar cantidad")
@@ -91,36 +96,44 @@ def carrito():
     #txt_producto = tkinter.Entry(ventana_carrito, textvariable = producto)
     txt_producto = tkinter.Entry(ventana_carrito)
     buscar_boton = tkinter.Button(ventana_carrito, text = "BUSCAR", bg = "gray64", command = lambda: buscar(txt_producto.get()))
+    
     lista_productos = tkinter.Listbox(ventana_carrito)
     lista_cantidades = tkinter.Listbox(ventana_carrito)
     lista_precios = tkinter.Listbox(ventana_carrito) #AGREGADO
+    
     eliminar_label = tkinter.Label(ventana_carrito, text = "Posición a eliminar")
     eliminar_entry = tkinter.Spinbox(ventana_carrito)   
     eliminar_boton = tkinter.Button(ventana_carrito, text = "ELIMINAR", command = eliminar_productosycantidades, bg = "red")
+    
     catalogo_boton = tkinter.Button(ventana_carrito, text = "CATALOGO", command = catalogo, bg = "SkyBlue1")
 
     #COLOCACION DE WIDGETS EN PANTALLA
 
-    titulo_label.grid(row = 0, column = 3)
-    total_label.grid(row = 1, column = 6)
-    total_monto_label.grid(row = 1, column= 7)
-    lista_label.grid(row = 2, column = 1)
-    cantidades_label.grid(row = 2, column = 2)
-    #precio_venta_label.grid()
-    agregar_producto_boton.grid(row = 4, column = 7)
-    #agregar_cantidad_boton.grid(row = 5, column = 6)
-    buscar_boton.grid(row = 4, column = 6)
-    ingresar_label.grid(row = 4, column = 3)
-    cantidad_label.grid(row = 5, column = 3)
-    cantidad_entry.grid(row = 5, column = 5)
-    txt_producto.grid(row = 4, column = 5)
-    lista_productos.grid(row = 5, column = 1)
-    lista_cantidades.grid(row = 5, column = 2)
-    lista_precios.grid(row = 9, column = 1) #AGREGADO
-    eliminar_label.grid(row = 7, column = 1)
-    eliminar_entry.grid(row = 7, column = 2)
-    eliminar_boton.grid(row = 7, column = 3)
-    catalogo_boton.grid(row = 8, column = 7)
+    titulo_label.grid(row = 0, column = 3) #TITULO DE LA VENTADA
+    total_label.grid(row = 1, column = 6) #TEXTO TOTAL A PAGAR
+    total_monto_label.grid(row = 1, column= 7) #CAJA DONDE SE MUESTRA EL MONTO TOTAL A PAGAR
+
+    lista_label.grid(row = 2, column = 1) #TEXTO "LISTA DE PRODUCTOS"
+    cantidades_label.grid(row = 2, column = 2) #TEXTO "CANTIDADES"
+    lista_parciales.grid(row = 2, column = 3) #TEXTO "montos parciales"
+    
+    agregar_producto_boton.grid(row = 4, column = 8) #BOTON PARA AGREGAR PRODUCTO    
+    buscar_boton.grid(row = 4, column = 7) #BOTON PARA BUSCAR
+
+    ingresar_label.grid(row = 4, column = 5) #TEXTO INGRESAR PRODUCTO
+    txt_producto.grid(row = 4, column = 6) #CAMPO PARA INGRESAR PRODUCTO
+
+    cantidad_label.grid(row = 5, column = 5) #TEXTO INGRESAR CANTIDAD
+    cantidad_entry.grid(row = 5, column = 6) #CAMPO PARA INGRESAR CANTIDAD
+    
+    lista_productos.grid(row = 5, column = 1) #LISTADO DE PRODUCTOS
+    lista_cantidades.grid(row = 5, column = 2) #LISTADO DE CANTIDADES
+    lista_precios.grid(row = 5, column = 3) #LISTADO DE PRECIOS
+
+    eliminar_label.grid(row = 10, column = 1) #TEXTO "INGRESE POSICION A ELIMINAR"
+    eliminar_entry.grid(row = 10, column = 2) #CAJA PARA INGRESAR EL INDICE A ELIMINAR
+    eliminar_boton.grid(row = 10, column = 3) #BOTON ELIMINAR
+    catalogo_boton.grid(row = 9, column = 7) #BOTON DE CATALOGO
 
 
 
