14be39d8f5b7207dc4c3a71907608eaeac8cea7b
MaxiC0303 <maximiliano.ecamano@gmail.com>
2022-12-25 01:58:21 -0300

    EXTRAS

--- Gui/tk_Funciones.py
+++ Gui/tk_Funciones.py
@@ -3,10 +3,7 @@ from tkinter import *
 from Database import sql
 
 
-def crear_producto():
-    db = sql.DataBase("superpy.db")
-    producto = db.insert("producto","id_producto,id_categoria,codigo,nombre,precio_venta",
-    )
+ 
 
 
 
@@ -21,6 +18,7 @@ def informacion_productos():
 
 
     #CREACION DE WIDGETS
+    id_producto_label = tkinter.Label(ventana_productos, text = "Ingrese ID del producto")
     codigo_label = tkinter.Label(ventana_productos, text = "Ingrese el codigo del producto: ")
     nombre_label = tkinter.Label(ventana_productos, text = "Ingrese el nombre del producto: ")
     precio_label = tkinter.Label(ventana_productos, text = "Ingrese el precio del prdocuto: ")
@@ -28,6 +26,14 @@ def informacion_productos():
     descripcion_label = tkinter.Label(ventana_productos, text = "Ingrese la descripciòn: ")
     categoria_label = tkinter.Label(ventana_productos, text ="Ingrese la categorìa del producto")
 
+    def crear_producto():
+        db = sql.DataBase("superpy.db")
+        db.insert("producto","id_producto,id_categoria,codigo,nombre,precio_venta", f"{id_producto_entry},'{categoria_entry}','{codigo_entry}',{nombre_entry}','{precio_entry}'")
+        db.close()
+
+    cargar_producto = tkinter.Button(ventana_productos, text = "CREAR", command = crear_producto)
+
+    id_producto_entry = tkinter.Entry(ventana_productos)
     codigo_entry = tkinter.Entry(ventana_productos)
     nombre_entry = tkinter.Entry(ventana_productos)
     precio_entry = tkinter.Entry(ventana_productos)
@@ -41,19 +47,21 @@ def informacion_productos():
     descripcion_categorias_listbox = tkinter.Listbox(ventana_productos)#LISTA DE PRECIOS EN CATALOGO
     idcategorias_listbox = tkinter.Listbox(ventana_productos)
     #COLOCACION DE WIDGETS EN PANTALLA
-    codigo_label.grid(row = 1, column = 1)
-    nombre_label.grid(row = 2, column = 1)
-    precio_label.grid(row = 3, column = 1)
-    stock_label.grid(row = 4, column = 1)
-    descripcion_label.grid(row = 5, column = 1) 
+    id_producto_label.grid(row = 1, column = 1)
+    codigo_label.grid(row = 2, column = 1)
+    nombre_label.grid(row = 3, column = 1)
+    precio_label.grid(row = 4, column = 1)
+    stock_label.grid(row = 5, column = 1)
+    descripcion_label.grid(row = 6, column = 1) 
 
-    codigo_entry.grid(row = 1, column = 2) 
-    nombre_entry.grid(row = 2, column = 2)
-    precio_entry.grid(row = 3, column = 2) 
-    stock_entry.grid(row = 4, column = 2) 
-    descripcion.grid(row = 5, column = 2) 
-    categoria_label.grid(row = 6, column = 1) 
-    categoria_entry.grid(row = 6, column = 2)
+    id_producto_entry.grid(row = 1, column = 2)
+    codigo_entry.grid(row = 2, column = 2) 
+    nombre_entry.grid(row = 3, column = 2)
+    precio_entry.grid(row = 4, column = 2) 
+    stock_entry.grid(row = 5, column = 2) 
+    descripcion.grid(row = 6, column = 2) 
+    categoria_label.grid(row = 7, column = 1) 
+    categoria_entry.grid(row = 7, column = 2)
 
     id_categorias_label.grid(row = 8, column = 1)
     categorìas_label.grid(row = 8, column = 2)
@@ -62,7 +70,7 @@ def informacion_productos():
     idcategorias_listbox.grid(row = 9, column = 1)
     categorìas_listbox.grid(row = 9, column = 2)    
     descripcion_categorias_listbox.grid(row = 9, column = 3)
-    
+    cargar_producto.grid(row = 10, column = 7)
     
 
     db = sql.DataBase('superpy.db')
@@ -81,5 +89,9 @@ def informacion_productos():
         descripcion_categorias_listbox.insert(END, categoria[2])
 ####
 
+    
+    
+    
     ventana_productos.mainloop()
 
+    
