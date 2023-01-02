a2125e5e06981f8ce9fd50fa3b75fdbf2f23c763
MaxiC0303 <maximiliano.ecamano@gmail.com>
2023-01-01 22:04:44 -0300

    funcionalidad_extras

--- Gui/tk_Carrito.py
+++ Gui/tk_Carrito.py
@@ -5,26 +5,17 @@ from tkinter import messagebox
 from Gui.tk_Catalogo import catalogo
 from tkinter.font import Font
 from tkinter import ttk
-from datetime import datetime
 from datetime import date
-
 from tkinter import messagebox
-#from Gui.tk_Terminar import terminar #ELIMINAR
-
 
 def carrito():    
     ventana_carrito = tkinter.Tk() #eliminar toplevel
     ventana_carrito.geometry("900x600")
     ventana_carrito.title("Carro de compras")
     ventana_carrito.configure(bg = "blue4")
-    #resultado = 0 
-    #DEFINICION DE FUNCIONES
-    #global resultado_total
-    ##resultado = StringVar()
-    ##precio_venta = int()
-    global total_monto_label #ELIMINAR
-        
-        
+    
+    #DEFINICION DE FUNCIONES    
+    global total_monto_label #ELIMINAR      
     
     def agregar_productosycantidades(producto):
         global resultado_total
@@ -33,33 +24,25 @@ def carrito():
         lista_parciales.config(state="normal")  #ACTIVA EL CAMPO PARA PODER HACER EL INGRESO
         total_monto_label.config(state="normal")
 
-        #resultado_total = 0
         if buscar(producto) == True:
             lista_productos.insert(END, txt_producto.get())           
-            lista_cantidades.insert(END, cantidad_entry.get())
-                      
+            lista_cantidades.insert(END, cantidad_entry.get())                      
             db = sql.DataBase("superpy.db")
             precio_venta = db.select("producto","precio_venta", f"nombre = '{txt_producto.get()}'")
             resultado = 0
             resultado_total = 0            
             resultado = resultado + float(precio_venta[0][0])*float(cantidad_entry.get())
-            lista_parciales.insert(END, resultado)
-                        
+            lista_parciales.insert(END, resultado)                        
             resultado_total = resultado_total + resultado
             total = float(total_monto_label.get()) + float(precio_venta[0][0])*float(cantidad_entry.get())
             total_monto_label.delete(0,END)
-            total_monto_label.insert(0,total)            
-           
+            total_monto_label.insert(0,total)         
+
+           #EN ESTE SECTOR SE DESACTIVA LOS CAMPOS.
             lista_productos.config(state="disabled") #GRISA EL CAMPO PARA VOLVER A DEJARLO BLOQUEADO HASTA EL PROXIMO EVENTO
             lista_cantidades.config(state="disabled") #GRISA EL CAMPO PARA VOLVER A DEJARLO BLOQUEADO HASTA EL PROXIMO EVENTO
             lista_parciales.config(state="disabled") #GRISA EL CAMPO PARA VOLVER A DEJARLO BLOQUEADO HASTA EL PROXIMO EVENTO
             total_monto_label.config(state="disabled")
-
-  #  def itemseleccionado(): #ELIMINAR NO FUNCIONA
-   #     for item in catalogo_lista_producto():
-    #        LB = tkinter.Label(ventana_carrito, text = catalogo_lista_producto.get(item)).pack()
-
-
 
     def eliminar_productosycantidades():#ESTA FUNCION PERMITE ELIMINAR PRODUCTOS Y CANTIDADES AL MISMO TIEMPO
         indice = int(eliminar_entry.get())
@@ -67,29 +50,22 @@ def carrito():
         lista_cantidades.config(state="normal") #ACTIVA EL CAMPO PARA PODER ELIMINAR
         lista_parciales.config(state="normal")  #ACTIVA EL CAMPO PARA PODER ELIMINAR
         total_monto_label.config(state="normal")
-
-        lista_productos.delete(indice - 1)
-        lista_cantidades.delete(indice - 1)
-        #lista_parciales.delete(indice - 1) ELIMINAR
         
-        #montoeliminado = lista_cantidades.delete(indice - 1) ELIMINAR
-        #print(montoeliminado)    ELIMINAR
-        #lista_parciales.delete(indice - 1) #ELIMINAR
-
+        lista_productos.delete(indice - 1)#ESTE CODIGO ELIMINA EL ITEM "INDICE-1" DE LA LISTA
+        lista_cantidades.delete(indice - 1)#ESTE CODIGO ELIMINA EL ITEM "INDICE-1" DE LA LISTA
+        
         montoeliminado = lista_parciales.get(indice - 1) #1º) DESIGNA EL VALOR A ELIMINAR COMO MONTOELIMINADO
-        
         total = float(total_monto_label.get()) - float(montoeliminado) #2º) SE UTILIZA EL MONTO ELIMINADO ANTERIOR PARA HACER LA RESTA
-        
+    
         total_monto_label.delete(0,END)#NUEVO
-        total_monto_label.insert(0,total)   #NUEVO
-        
+        total_monto_label.insert(0,total)   #NUEVO        
 
         lista_parciales.delete(indice - 1) #3º) RECIEN AHORA SE PROCEDE A ELIMINAR DICHO MONTO
 
         lista_productos.config(state="disabled") #GRISA EL CAMPO PARA VOLVER A DEJARLO BLOQUEADO HASTA EL PROXIMO EVENTO
         lista_cantidades.config(state="disabled") #GRISA EL CAMPO PARA VOLVER A DEJARLO BLOQUEADO HASTA EL PROXIMO EVENTO
         lista_parciales.config(state="disabled") #GRISA EL CAMPO PARA VOLVER A DEJARLO BLOQUEADO HASTA EL PROXIMO EVENTO
-        total_monto_label.config(state="disabled")
+        total_monto_label.config(state="disabled")#GRISA EL CAMPO PARA VOLVER A DEJARLO BLOQUEADO HASTA EL PROXIMO EVENTO
 
 
     def buscar(producto):
@@ -110,50 +86,29 @@ def carrito():
             messagebox.showinfo("Producto no valido", "Ingrese un producto válido")
             return False
             
-    #def buscar(producto): 
-            #db = sql.DataBase("superpy.db")
-            #producto_valido = db.select("usuario", "email,password",f'email = {email}',f'password = {password}')
-
-        # if email == usuario_valido[0][0]:
-            #    if password == usuario_valido[0][1]:
-            #       tkinter.messagebox.showinfo(title="Inicio", message="Inicio de sesión exitoso")
-            #db.close() #siempre cerrar la conexión a la base de datos
-
     ##CREACION DE WIDGETS  
-    #producto = StringVar()
     titulo_label = tkinter.Label(ventana_carrito, text = "Carro de compras")
     total_label = tkinter.Label(ventana_carrito, text = "Total a pagar $")
     total_monto_label = tkinter.Entry(ventana_carrito)
     total_monto_label.insert(0,"0.0")
     total_monto_label.config(state="disabled")
-
     lista_label = tkinter.Label(ventana_carrito, text = "Lista de productos")
     cantidades_label = tkinter.Label(ventana_carrito, text = "Cantidades")
     label_parciales = tkinter.Label(ventana_carrito, text = "Monto Parcial")
-
     agregar_producto_boton = tkinter.Button(ventana_carrito, text = "AGREGAR", command = lambda: agregar_productosycantidades(txt_producto.get()), bg = "medium orchid")
-    #agregar_producto_boton = tkinter.Button(ventana_carrito, text = "AGREGAR", command = itemseleccionado)
-    
-    #agregar_cantidad_boton = tkinter.Button(ventana_carrito, text = "AGREGAR Q", command = agregar_cantidad)
     ingresar_label = tkinter.Label(ventana_carrito , text = "Ingresar un producto")
     cantidad_label = tkinter.Label(ventana_carrito , text = "Ingresar cantidad")
     cantidad_entry = tkinter.Spinbox(ventana_carrito, from_=0, to=1000, increment=1)
-    #txt_producto = tkinter.Entry(ventana_carrito, textvariable = producto)
     txt_producto = tkinter.Entry(ventana_carrito)
-    buscar_boton = tkinter.Button(ventana_carrito, text = "BUSCAR", bg = "gray64", command = lambda: buscar(txt_producto.get()))
-    
+    buscar_boton = tkinter.Button(ventana_carrito, text = "BUSCAR", bg = "gray64", command = lambda: buscar(txt_producto.get())) 
     lista_productos = tkinter.Listbox(ventana_carrito)
     lista_cantidades = tkinter.Listbox(ventana_carrito)
-    lista_parciales = tkinter.Listbox(ventana_carrito) #AGREGADO
-    
+    lista_parciales = tkinter.Listbox(ventana_carrito) #AGREGADO    
     eliminar_label = tkinter.Label(ventana_carrito, text = "Posición a eliminar")
     eliminar_entry = tkinter.Spinbox(ventana_carrito, from_=0, to=1000, increment=1)   
     eliminar_boton = tkinter.Button(ventana_carrito, text = "ELIMINAR", command = eliminar_productosycantidades, bg = "red")
-    
     catalogo_boton = tkinter.Button(ventana_carrito, text = "CATALOGO", command = catalogo, bg = "SkyBlue1")
-
     terminar_boton = tkinter.Button(ventana_carrito, text = "TERMINAR", command = terminar)
-
     catalogo_tree = ttk.Treeview(ventana_carrito, column = ("Catalogo Productos","Precios", "Stock"), show='headings', selectmode="browse")
     catalogo_tree.column("# 1", anchor=CENTER, width= 120)
     catalogo_tree.heading("# 1", text="Catalogo productos")
@@ -193,12 +148,10 @@ def carrito():
     terminar_boton.grid(row = 14, column = 10)
     catalogo_tree.place(x=0, y=330)
 
-    #####
+    #LISTA DE PRODUCTOS EN LA BASE DE DATOS
     db = sql.DataBase("superpy.db")
     producto_valido = db.select_all("producto","nombre,precio_venta,stock")
-    ######
-   # db = sql.DataBase('superpy.db')
-    #categorias = db.select_all("categoria","id_categoria,nombre,descripcion")
+    
     for producto_val in producto_valido:
         catalogo_tree.insert('','end', text="1",values=((producto_val[0], producto_val[1], producto_val[2])))
 
@@ -207,43 +160,13 @@ def carrito():
     catalogo_tree.configure(yscrollcommand=catalogo_treeScrollbar.set)
     catalogo_treeScrollbar.place(x=0+230,y=330, height=225)
 
-    ######
-
-    #catalogo_productos = tkinter.Label(ventana_carrito, text = "Catalogo de productos")
-    #precios_productos = tkinter.Label(ventana_carrito, text = "Precios unitarios")
-
-    #catalogo_lista_producto = tkinter.Listbox(ventana_carrito)# LISTA DE PRODUCTOS EN CATALOGO
-    
-    #catalogo_lista_precio = tkinter.Listbox(ventana_carrito)#LISTA DE PRECIOS EN CATALOGO
-
-    #for producto in producto_valido:
-     #   catalogo_lista_producto.insert(END, producto[0])
-
-    #for producto in producto_valido:
-     #   catalogo_lista_precio.insert(END, producto[1])
-
-
-    #catalogo_lista_producto.config(state="disabled") #ESTO PERMITE GRISAR LA LISTA DE CATALOGOS
-    #catalogo_lista_precio.config(state="disabled")  #ESTO PERMITE GRISAR LA LISTA DE CATALOGOS
-
-    
-    #WIDGETS EN PANTALLA
-
-    #atras_boton.grid(row = 1, column = 1)
-    #catalogo_lista_producto.grid(row = 13, column = 1)
-    #catalogo_lista_precio.grid(row = 13, column = 2)
-    #catalogo_productos.grid(row = 12, column = 1)
-    #precios_productos.grid(row = 12, column = 2)
-
-
     ventana_carrito.mainloop()
 
 def terminar():
     ventana_terminar = tkinter.Toplevel()
     ventana_terminar.geometry("800x400")
     ventana_terminar.title("Ventana Principal")
-    ventana_terminar.configure(bg = "blue4")
-    
+    ventana_terminar.configure(bg = "blue4")    
 
     pagar = tkinter.Entry(ventana_terminar, font=Font(size=15))
     pagar.config(state="normal")
@@ -252,11 +175,9 @@ def terminar():
 
     texto_gracias = tkinter.Label(ventana_terminar, text = "Muchas gracias por elegirnos", font=Font(size=15))
     texto_pagar = tkinter.Label(ventana_terminar, text = "Su monto total a pagar es de: $")
-    
-    finalizar_venta = tkinter.Button(ventana_terminar, text="FINALIZAR",command=finalizar)
-    
+    finalizar_venta = tkinter.Button(ventana_terminar, text="FINALIZAR",command=finalizar)  
 
-          
+    #COLOCACION DE WIDGETS EN PANTALLA      
     pagar.place(x=200, y =100)
     texto_gracias.place(x=200, y=0)
     texto_pagar.place(x=0, y=100)
@@ -269,39 +190,42 @@ def finalizar():
     ventana_finalizar.title("Ventas")
     ventana_finalizar.configure(bg = "blue4")    
     
-    def cargar_venta():
-        
+    def cargar_venta():        
         db = sql.DataBase("superpy.db") 
-        if len(tipocomprobante_entry.get()) > 0 and len(comprobante_entry.get()) > 0 and len(fecha_entry.get()) > 0 and len(total_monto_label.get()) > 0:
+        if len(tipocomprobante_entry.get()) > 0 and len(comprobante_entry.get()) > 0 and len(fecha_entry.get()) > 0 and len(total_monto_label.get()) > 0 and len(cliente_entry.get()):
             db = sql.DataBase("superpy.db")
-            db.insert('venta','tipo_comprobante,nro_comprobante,fecha,total', f'"{tipocomprobante_entry.get()}","{comprobante_entry.get()}","{fecha_entry.get()}","{total_monto_label.get()}"')
+            db.insert('venta','tipo_comprobante,nro_comprobante,fecha,total,id_cliente', f'"{tipocomprobante_entry.get()}","{comprobante_entry.get()}","{fecha_entry.get()}","{total_monto_label.get()}","{cliente_entry.get()}"')
             messagebox.showinfo("Agregado", "Venta generada.")
             db.close()  
         else:
             messagebox.showerror("ERROR", "Algo salió mal, la venta no se ha generado .")
 
-    tipocomprobante_label = tkinter.Label(ventana_finalizar, text = "Ingrese el tipo de comrobante")
+    #DEFINICION DE WIDGETS
+    tipocomprobante_label = tkinter.Label(ventana_finalizar, text = "Ingrese el tipo de comprobante")
     tipocomprobante_entry=tkinter.Entry(ventana_finalizar)
     comprobante_label = tkinter.Label(ventana_finalizar, text = "Ingrese n° de comprobante")
     comprobante_entry=tkinter.Entry(ventana_finalizar)
+    cliente_label=tkinter.Label(ventana_finalizar, text="Ingrese el nombre y apellido del cliente: ")
+    cliente_entry=tkinter.Entry(ventana_finalizar)
     fecha = tkinter.Label(ventana_finalizar, text = "Fecha")
     fecha_entry = tkinter.Entry(ventana_finalizar)
-    dia = date.today()
-    
+    dia = date.today()    
     fecha_entry.insert(END,dia)
     print(fecha_entry)
     total_entry = tkinter.Entry(ventana_finalizar)
     total_entry.insert(END, total_monto_label)
     cargar_venta_boton=tkinter.Button(ventana_finalizar,text= "CARGAR VENTA", command=cargar_venta)
 
+    #COLOCACION DE WIDGETS EN PANTALLA
     tipocomprobante_label.grid(row=1,column=1)
     tipocomprobante_entry.grid(row=1,column=2)
     comprobante_label.grid(row=2,column=1)
     comprobante_entry.grid(row=2,column=2)
     fecha.grid(row=3,column=1)
     fecha_entry.grid(row=3,column=2)
-    cargar_venta_boton.grid(row=4,column=1)
-    
+    cliente_label.grid(row=4,column=1)
+    cliente_entry.grid(row=4,column=2)
+    cargar_venta_boton.grid(row=5,column=1)    
     
     ventana_finalizar.mainloop()
 
--- Gui/tk_Ventas.py
+++ Gui/tk_Ventas.py
@@ -5,11 +5,11 @@ from tkinter import ttk
 
 def ventas():
     ventana_ventas= tkinter.Tk() 
-    ventana_ventas.geometry("600x400")
+    ventana_ventas.geometry("1000x400")
     ventana_ventas.title("Informacion relevante para el producto")
     ventana_ventas.configure(bg = "blue4")     
         
-    tree_ventas = ttk.Treeview(ventana_ventas, column = ("IDD venta","Tipo de Comprobante","N° de Comprobante","Fecha","Total", "Estado"), show='headings',height=4)
+    tree_ventas = ttk.Treeview(ventana_ventas, column = ("IDD venta","Tipo de Comprobante","N° de Comprobante","Fecha","Total", "Estado","ID Cliente"), show='headings',height=4)
 
     # Add a Treeview widget CORRESPONDE AL TREEVIEW DE LOS PRODUCTOS
     tree_ventas.column("# 1", anchor=CENTER, width= 60)
@@ -24,6 +24,10 @@ def ventas():
     tree_ventas.heading("# 5", text="Total")
     tree_ventas.column("# 6", anchor=CENTER, width= 60)
     tree_ventas.heading("# 6", text="Estado")
+    tree_ventas.column("# 7", anchor=CENTER, width= 120)
+    tree_ventas.heading("# 7", text="Cliente")
+
+
 
     presentacion_ventas=tkinter.Label(ventana_ventas,text="Resumen de Ventas")
     
@@ -32,10 +36,10 @@ def ventas():
     tree_ventas.grid(row=3,column=1)
 
     db = sql.DataBase("superpy.db")
-    ventas = db.select_all("venta","id_venta,tipo_comprobante,nro_comprobante,fecha,total,estado")
+    ventas = db.select_all("venta","id_venta,tipo_comprobante,nro_comprobante,fecha,total,estado,id_cliente")
 
     for venta in ventas:
-        tree_ventas.insert('','end', text="1",values=((venta[0], venta[1], venta[2], venta[3], venta[4], venta[5])))     
+        tree_ventas.insert('','end', text="1",values=((venta[0], venta[1], venta[2], venta[3], venta[4], venta[5],venta[6])))     
     
     ventana_ventas.mainloop()
 
